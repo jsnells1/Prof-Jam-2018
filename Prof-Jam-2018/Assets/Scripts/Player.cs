edea38f63dda5425890cc492bbf2d75b2d36f06f
jsnellings1 <jsnellings1@gulls.salisbury.edu>
2018-09-29 13:22:31 -0400

    Imported Humanoid

---
+++ Prof-Jam-2018/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform foot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foot = gameObject.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.LeftFoot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foot.Rotate(Vector3.down * Time.deltaTime * 20);
+    }
+}
