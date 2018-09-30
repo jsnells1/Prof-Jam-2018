ab9014b7a56015c8bf3ac6a9438edd66d3b3bdbd
PureUnstable <pureunstable@gmail.com>
2018-09-30 02:58:27 -0400

    I am Alpha and Omega
    War, war never changes

---
+++ Prof-Jam-2018/Assets/Scripts/ThugLyfe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThugLyfe : MonoBehaviour
+{
+    bool isMoving;
+    public float moveSpeed;
+    float lerpPosition = 0;
+    Vector3 initialPosition;
+    public List<SpriteRenderer> glassesParts;
+    public Transform glasses;
+    public Transform glassesFacePosition;
+
+    private void Update()
+    {
+        //if (glasses.Translate(Mathf.Lerp(initialPosition.y, 0f, Time.deltaTime * moveSpeed));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            YoHoYoHoThugishLyfesForMe();
+    }
+
+    void YoHoYoHoThugishLyfesForMe()
+    {
+        foreach(SpriteRenderer sr in glassesParts)
+        {
+            sr.enabled = true;
+        }
+
+        isMoving = true;
+        initialPosition = glasses.position;
+    }
+}
